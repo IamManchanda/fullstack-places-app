b3b0aba995bf4d6bfd53c16896e148158a291e31
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-01 16:43:08 +0530

    Frontend: Code Splitting & Lazy Loading

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-pascal-case */
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -7,16 +7,22 @@ import {
   Switch,
 } from "react-router-dom";
 import Navigation_MainNavigation from "./components/navigation/main-navigation";
-import P_Index from "./pages";
-import P_Auth_Login from "./pages/auth/login";
-import P_Auth_Signup from "./pages/auth/signup";
-import P_Users_UserId_Places from "./pages/users/_user_id/places";
-import P_Places_PlaceId_Edit from "./pages/places/_place_id/edit";
-import P_Places_New from "./pages/places/new";
-import P_Error from "./pages/error";
+import Shared_LoadingSpinner from "./components/shared/loading-spinner";
 import { AuthProvider } from "./context/auth";
 import { useAuth } from "./hooks/auth";
 import "./assets/styles.css";
+
+const P_Index = lazy(() => import("./pages"));
+const P_Auth_Login = lazy(() => import("./pages/auth/login"));
+const P_Auth_Signup = lazy(() => import("./pages/auth/signup"));
+const P_Users_UserId_Places = lazy(() =>
+  import("./pages/users/_user_id/places"),
+);
+const P_Places_PlaceId_Edit = lazy(() =>
+  import("./pages/places/_place_id/edit"),
+);
+const P_Places_New = lazy(() => import("./pages/places/new"));
+const P_Error = lazy(() => import("./pages/error"));
 
 const App = () => {
   const [token, login, logout, userId] = useAuth();
@@ -27,45 +33,53 @@ const App = () => {
       <Router>
         <Navigation_MainNavigation />
         <main>
-          {token ? (
-            <Switch>
-              <Route path="/" exact>
-                <P_Index />
-              </Route>
-              <Route path="/users/:userId/places" exact>
-                <P_Users_UserId_Places />
-              </Route>
-              <Route path="/places/:placeId/edit" exact>
-                <P_Places_PlaceId_Edit />
-              </Route>
-              <Route path="/places/new" exact>
-                <P_Places_New />
-              </Route>
-              <Route path="/error" exact>
-                <P_Error />
-              </Route>
-              <Redirect to="/error" />
-            </Switch>
-          ) : (
-            <Switch>
-              <Route path="/" exact>
-                <P_Index />
-              </Route>
-              <Route path="/users/:userId/places" exact>
-                <P_Users_UserId_Places />
-              </Route>
-              <Route path="/auth/login" exact>
-                <P_Auth_Login />
-              </Route>
-              <Route path="/auth/signup" exact>
-                <P_Auth_Signup />
-              </Route>
-              <Route path="/error" exact>
-                <P_Error />
-              </Route>
-              <Redirect to="/error" />
-            </Switch>
-          )}
+          <Suspense
+            fallback={
+              <div className="center">
+                <Shared_LoadingSpinner />
+              </div>
+            }
+          >
+            {token ? (
+              <Switch>
+                <Route path="/" exact>
+                  <P_Index />
+                </Route>
+                <Route path="/users/:userId/places" exact>
+                  <P_Users_UserId_Places />
+                </Route>
+                <Route path="/places/:placeId/edit" exact>
+                  <P_Places_PlaceId_Edit />
+                </Route>
+                <Route path="/places/new" exact>
+                  <P_Places_New />
+                </Route>
+                <Route path="/error" exact>
+                  <P_Error />
+                </Route>
+                <Redirect to="/error" />
+              </Switch>
+            ) : (
+              <Switch>
+                <Route path="/" exact>
+                  <P_Index />
+                </Route>
+                <Route path="/users/:userId/places" exact>
+                  <P_Users_UserId_Places />
+                </Route>
+                <Route path="/auth/login" exact>
+                  <P_Auth_Login />
+                </Route>
+                <Route path="/auth/signup" exact>
+                  <P_Auth_Signup />
+                </Route>
+                <Route path="/error" exact>
+                  <P_Error />
+                </Route>
+                <Redirect to="/error" />
+              </Switch>
+            )}
+          </Suspense>
         </main>
       </Router>
     </AuthProvider>
