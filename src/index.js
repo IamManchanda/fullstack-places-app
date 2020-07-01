77edfbeb8b4348e72b048daf42232f0e2dfb9bdc
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-01 14:27:51 +0530

    Refactor: File Restructure!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./app";
+import ClientApp from "./client-app";
 import * as serviceWorker from "./serviceWorker";
 
 /* eslint-disable no-extend-native */
 Number.prototype[Symbol.iterator] = function* () {
   for (let i = 1; i <= this; i += 1) {
     yield i;
-  };
+  }
 };
 /* eslint-enable */
 
-render(<App />, document.getElementById("root"));
+render(<ClientApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
